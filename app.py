b466a1e4cbf1862b2c87dab2fcb618d6f6e2ad5d
David Wang <david.wang34@gmail.com>
2022-08-04 15:33:49 -0700

    add some relay control buttons

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
 import datetime
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+import uuid
 
 app = Flask(__name__)
-GPIO.setmode(GPIO.BCM)
+app.secret_key = str(uuid.uuid4())
+# GPIO.setmode(GPIO.BCM)
 
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-    23: {'name': 'GPIO 23', 'state': GPIO.LOW},
-    24: {'name': 'GPIO 24', 'state': GPIO.LOW},
-    25: {'name': 'GPIO 25', 'state': GPIO.LOW}
+# create dictionary for what drinks are connected to which tubes
+liquid_sources_dict = {
+    'rum': 1,
+    'coke': 2
 }
 
-for pin in pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
+# for pin in [6, 13]:
+#     GPIO.setup(pin, GPIO.OUT)
+#     GPIO.output(pin, GPIO.LOW)
 
 # http://mattrichardson.com/Raspberry-Pi-Flask/
 # https://learn.adafruit.com/adafruit-keg-bot?view=all
@@ -31,9 +32,8 @@ web_title = "Welcome to 409!"
 @app.route("/")
 def index():
     now = datetime.datetime.now()
-    GPIO.output(23, GPIO.LOW)
-    GPIO.output(24, GPIO.LOW)
-    GPIO.output(25, GPIO.LOW)
+    # GPIO.output(6, GPIO.LOW)
+    # GPIO.output(13, GPIO.LOW)
     time_string = now.strftime("%Y-%m-%d %H:%M")
     template_data = {
         'title': web_title,
@@ -45,7 +45,7 @@ def index():
 @app.route("/confirmation/<drink>")
 def confirmation(drink):
     if drink == 'rum-and-coke':
-        GPIO.output(23, GPIO.HIGH)
+        # GPIO.output(23, GPIO.HIGH)
         template_data = {
             'title': web_title,
             'drink': "rum and coke"
@@ -54,7 +54,7 @@ def confirmation(drink):
         return render_template('confirmation.html', **template_data)
 
     if drink == 'rainforest':
-        GPIO.output(24, GPIO.HIGH)
+        # GPIO.output(24, GPIO.HIGH)
         template_data = {
             'title': web_title,
             'drink': "rainforest"
@@ -62,12 +62,56 @@ def confirmation(drink):
         return render_template('confirmation.html', **template_data)
 
     if drink == 'gin-and-tonic':
-        GPIO.output(25, GPIO.HIGH)
+        # GPIO.output(25, GPIO.HIGH)
         template_data = {
             'title': web_title,
             'drink': "gin and tonic"
         }
         return render_template('confirmation.html', **template_data)
+
+
+@app.route("/relay-1/start")
+def relay_1_start():
+    # GPIO.output(6, GPIO.HIGH)
+    flash("relay 1 has started")
+    return redirect('/')
+
+
+@app.route("/relay-1/stop")
+def relay_1_stop():
+    # GPIO.output(6, GPIO.LOW)
+    flash("relay 1 has stopped")
+    return redirect('/')
+
+
+@app.route("/relay-2/start")
+def relay_2_start():
+    # GPIO.output(13, GPIO.HIGH)
+    flash("relay 2 has started")
+    return redirect('/')
+
+
+@app.route("/relay-2/stop")
+def relay_2_stop():
+    # GPIO.output(13, GPIO.LOW)
+    flash("relay 2 has stopped")
+    return redirect('/')
+
+
+@app.route("/relay-1-2/start")
+def relay_1_2_start():
+    # GPIO.output(6, GPIO.HIGH)
+    # GPIO.output(13, GPIO.HIGH)
+    flash("relay 1 and 2 has started")
+    return redirect('/')
+
+
+@app.route("/relay-1-2/stop")
+def relay_1_2_stop():
+    # GPIO.output(6, GPIO.LOW)
+    # GPIO.output(13, GPIO.LOW)
+    flash("relay 1 and 2 has stopped")
+    return redirect('/')
 
 
 @app.route("/rum-and-coke")
