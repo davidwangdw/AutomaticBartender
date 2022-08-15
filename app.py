2669247350035a6e2244f86c4225ad409d531da5
David Wang <david.wang34@gmail.com>
2022-08-14 21:12:30 -0700

    bug fix

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def make_drink(recipe):
     time_elapsed = 0
     longest_time_needed = max(recipe.values())
     while time_elapsed <= longest_time_needed:
-        relays_to_activate = [liquid_sources_dict[ingredient] for ingredient, time_left in recipe.items() if time_left > 0]
-        relays_to_deactivate = [liquid_sources_dict[ingredient] for ingredient, time_left in recipe.items() if time_left == 0]
+        relays_to_activate = [gpio_to_relay_dict[liquid_sources_dict[ingredient]] for ingredient, time_left in recipe.items() if time_left > 0]
+        relays_to_deactivate = [gpio_to_relay_dict[liquid_sources_dict[ingredient]] for ingredient, time_left in recipe.items() if time_left == 0]
         print(f'relays to activate: {relays_to_activate}')
         print(f'relays to deactivate: {relays_to_deactivate}')
         for relay in relays_to_deactivate:
